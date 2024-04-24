ac35d4028db09578d5905bd8b60cda6462ef6575
Qian Hui Tan <qianhui.tan@yale-nus.edu.sg>
2024-04-24 23:53:28 +0800

    Add sex of organism to sample data

--- R/sample_data.R
+++ R/sample_data.R
@@ -10,6 +10,7 @@
 #'   \item{day}{ Day of study}
 #'   \item{dead}{ Number of organisms dead on this day}
 #'   \item{censored}{ Number of organisms censored (missing, etc) on this day}
+#'   \item{sex}{ Sex of organism. M = Male, F = Female.}
 #'   ...
 #' }
 
